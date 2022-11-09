cd0076cd1f22f323b8edf7a3a6acb9f54eeb6650
TheDarkAngel099 <nagarjunamote@gmail.com>
2022-11-09 16:23:21 +0000

    Added collectable Nuclear Waste

---
+++ Assets/Scripts/Collector.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collector : MonoBehaviour
+{
+    public int points = 0;
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10,10,100,20), "Waste Collected : " + points);
+    }
+}
---
+++ Assets/Scripts/NucliarWaste.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucliarWaste : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Collector>().points++; //adding 1 point per nucliar waste
+            Destroy(gameObject); // destroying the pickup
+        }
+    }
+}
