950cbb6c1fbc78df9658ace642e8b8eb8d2ad40b
Cocornut <115100051+Cocornut@users.noreply.github.com>
2022-11-23 17:24:01 +0000

    Fixed issues with github

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -1,13 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Collector : MonoBehaviour
 {
     public int points = 0;
 
+    public int maxPoints;
+
+    //Counting the amount of Waste objects
+    void Start()
+    {
+        maxPoints = GameObject.FindGameObjectsWithTag("Waste").Length;
+        Debug.Log(maxPoints);
+    }
+
     private void OnGUI()
     {
-        GUI.Label(new Rect(10,10,300,20), "Waste Collected: " + points);
+        GUI.Label(new Rect(10, 10, 300, 20), "Waste Collected: " + points);
+    }
+
+    void Update()
+    {
+        // If there are no more pickups load the next scene
+        if (points >= maxPoints)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log("You beat this level");
+        }
     }
 }
