03e1c949f087cdc8ae142e9064bf5761163ef5a9
Cocornut <115100051+Cocornut@users.noreply.github.com>
2022-12-28 17:37:47 +0000

    Skybox and ground material

---
+++ Assets/Scripts/FogEffect.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class FogEffect : MonoBehaviour
+{
+    public Material _mat;
+    public Color _fogColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Camera>().depthTextureMode = DepthTextureMode.Depth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mat.SetColor("_FogColor", _fogColor);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture desination)
+    {
+        Graphics.Blit(source, desination, _mat);
+    }
+}
--- Assets/Scripts/UnderWaterEffect.cs
+++ Assets/Scripts/UnderWaterEffect.cs
@@ -6,6 +6,15 @@ using UnityEngine;
 public class UnderWaterEffect : MonoBehaviour
 {
     public Material _mat;
+
+    [Range(0.001f, 0.1f)]
+    public float _pixelOffset;
+    [Range(0.1f, 20f)]
+    public float _noiseScale;
+    [Range(0.1f, 20f)]
+    public float _noiseFrequency;
+    [Range(0.1f, 30f)]
+    public float _noiseSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +24,10 @@ public class UnderWaterEffect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        _mat.SetFloat("_NoiseFrequency", _noiseFrequency);
+        _mat.SetFloat("_NoiseSpeed", _noiseSpeed);
+        _mat.SetFloat("_NoiseScale", _noiseScale); ;
+        _mat.SetFloat("_Pixe;Offset", _pixelOffset);
     }
 
     private void OnRenderImage(RenderTexture source, RenderTexture desination)
